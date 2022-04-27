14e4b91876a48fef778d76cb61a04193141052ac
Chenyu <wchenyucy@126.com>
2022-04-27 22:01:26 +0800

    项目初始化

---
+++ Assets/Scripts/teleport.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public string currentScene;
+    public string transitionScene;
+}
